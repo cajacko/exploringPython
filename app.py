a684f2b6935f92148cebc5d30fa382e7565f8842
cajacko <charlie@Charless-MacBook-Pro.local>
2016-04-24 09:25:17 +0100

    Added and checked mysql works

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 '''Get config file'''
 
 import ConfigParser
+import MySQLdb
 
 CONFIG = ConfigParser.ConfigParser()
-CONFIG.read("config.ini")
+CONFIG.read('config.ini')
 
 def config_section_map(section):
     '''Return the sections of the config'''
@@ -13,4 +14,29 @@ def config_section_map(section):
         dict1[option] = CONFIG.get(section, option)
     return dict1
 
-print config_section_map("MySql")
+# print config_section_map('MySql')['host']
+
+HOST = config_section_map('MySql')['host']
+USER = config_section_map('MySql')['user']
+PASSWORD = config_section_map('MySql')['password']
+DATABASE = config_section_map('MySql')['database']
+
+DB = MySQLdb.connect(
+    host=HOST,
+    user=USER,
+    passwd=PASSWORD,
+    db=DATABASE
+)
+
+# You must create a Cursor object. It will let
+# you execute all the queries you need
+CUR = DB.cursor()
+
+# Use all the SQL you like
+CUR.execute('SELECT * FROM stories')
+
+# print all the first cell of all the rows
+for row in CUR.fetchall():
+    print row[0]
+
+DB.close()
